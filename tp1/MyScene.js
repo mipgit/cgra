6fee9bb6d35b9b10e476c07857ea80992d2190f5
Mariana Almeida <marianacabralmeida@gmail.com>
2026-02-26 12:13:48 +0000

    exercise 1

--- tp1/MyScene.js
+++ tp1/MyScene.js
@@ -1,5 +1,7 @@
 import { CGFscene, CGFcamera, CGFaxis } from "../lib/CGF.js";
 import { MyDiamond } from "./MyDiamond.js";
+import { MyTriangle } from "./MyTriangle.js";
+import { MyParallelogram } from "./MyParallelogram.js";
 
 /**
  * MyScene
@@ -16,7 +18,7 @@ export class MyScene extends CGFscene {
     this.initLights();
 
     //Background color
-    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    this.gl.clearColor(255.0, 255.0, 255.0, 1.0);
 
     this.gl.clearDepth(100.0);
     this.gl.enable(this.gl.DEPTH_TEST);
@@ -26,6 +28,11 @@ export class MyScene extends CGFscene {
     //Initialize scene objects
     this.axis = new CGFaxis(this);
     this.diamond = new MyDiamond(this);
+    this.displayDiamond = true;
+    this.triangle = new MyTriangle(this);
+    this.displayTriangle = true;
+    this.parallelogram = new MyParallelogram(this);
+    this.displayParallelogram = true;
 
     //Objects connected to MyInterface
     this.displayAxis = true;
@@ -65,6 +72,9 @@ export class MyScene extends CGFscene {
 
     // Draw axis
     if (this.displayAxis) this.axis.display();
+    if (this.displayDiamond) this.diamond.display();
+    if (this.displayTriangle) this.triangle.display();
+    if (this.displayParallelogram) this.parallelogram.display();
 
     this.setDefaultAppearance();
 
@@ -91,7 +101,9 @@ export class MyScene extends CGFscene {
 
     // ---- BEGIN Primitive drawing section
 
-    this.diamond.display();
+    if (this.displayDiamond) this.diamond.display();
+    if (this.displayTriangle) this.triangle.display();
+    if (this.displayParallelogram) this.parallelogram.display();
 
     // ---- END Primitive drawing section
   }
