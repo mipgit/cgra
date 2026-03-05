417d29ae7dd1d51f4f2fa0401b3eb60f2796956c
Mariana Almeida <marianacabralmeida@gmail.com>
2026-03-05 13:45:29 +0000

    added normais for lighting

--- tp2/MyParallelogram.js
+++ tp2/MyParallelogram.js
@@ -12,19 +12,27 @@ export class MyParallelogram extends CGFobject {
 	
 	initBuffers() {
 		this.vertices = [
-			0, 0, 0,	//0
-			1, 1, 0,	//1
-			3, 1, 0,	//2
-			2, 0, 0, 	//3
+			0, 0, 0,	//0 frente
+			1, 1, 0,	//1 frente
+			3, 1, 0,	//2 frente
+			2, 0, 0, 	//3 frente
+			0, 0, 0,	//4 trás
+			1, 1, 0,	//5 trás
+			3, 1, 0,	//6 trás
+			2, 0, 0, 	//7 trás
 		];
 
 		//Counter-clockwise reference of vertices
 		this.indices = [
 			0, 3, 1,
-			3, 2, 1, //lado 1
-			0, 1, 3,
-			3, 1, 2, //lado 2
+			3, 2, 1, //frente
+			4, 5, 7,
+			7, 5, 6, //trás
+		];
 
+		this.normals = [
+			0, 0,  1,  0, 0,  1,  0, 0,  1,  0, 0,  1,  // frente
+			0, 0, -1,  0, 0, -1,  0, 0, -1,  0, 0, -1,  // trás
 		];
 
 		//The defined indices (and corresponding vertices)
