4ad41103a6686303d3a4188c8686ad7a86465cfe
maria luiza <up202304306@edu.fe.up.pt>
2026-03-19 10:54:39 +0000

    removed unnecessary covers for prism

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -49,63 +49,12 @@ export class MyPrism extends CGFobject {
             // 2nd triangle
             this.indices.push(vCount + 1, vCount + 3, vCount + 2);
 
+            // inverted order
+            this.indices.push(vCount + 1, vCount, vCount + 2);
+            this.indices.push(vCount + 2, vCount + 3, vCount + 1);
+
             vCount += 4;
         }
-    }
-
-    // draw the top and bottom caps
-    // we need separate vertices for the caps because the normals are different (vertical vs horizontal)
-
-    // bottom cap (z = 0)
-    var bottomCenterIndex = this.vertices.length / 3;
-    
-    // push center vertex
-    this.vertices.push(0, 0, 0);
-    this.normals.push(0, 0, -1); 
-
-    // [ush perimeter vertices
-    for (var i = 0; i <= this.slices; i++) {
-        var ang = i * alphaAng;
-        var x = Math.cos(ang);
-        var y = Math.sin(ang);
-        
-        this.vertices.push(x, y, 0);
-        this.normals.push(0, 0, -1); // normal points down
-    }
-
-    // push indices
-    for (var i = 0; i < this.slices; i++) {
-        // center is at bottomCenterIndex
-        // the perimeter starts at bottomCenterIndex + 1
-        var p1 = bottomCenterIndex + 1 + i;
-        var p2 = bottomCenterIndex + 1 + i + 1;
-        
-        this.indices.push(bottomCenterIndex, p2, p1);
-    }
-
-    // top cap (z = 1)
-    var topCenterIndex = this.vertices.length / 3;
-
-    // push center vertex
-    this.vertices.push(0, 0, 1);
-    this.normals.push(0, 0, 1); // normal points up
-
-    // push perimeter vertices
-    for (var i = 0; i <= this.slices; i++) {
-        var ang = i * alphaAng;
-        var x = Math.cos(ang);
-        var y = Math.sin(ang);
-        
-        this.vertices.push(x, y, 1);
-        this.normals.push(0, 0, 1);
-    }
-
-    // push indices
-    for (var i = 0; i < this.slices; i++) {
-        var p1 = topCenterIndex + 1 + i;
-        var p2 = topCenterIndex + 1 + i + 1;
-
-        this.indices.push(topCenterIndex, p1, p2);
     }
 
     this.primitiveType = this.scene.gl.TRIANGLES;
