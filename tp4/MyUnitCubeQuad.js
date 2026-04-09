ff7936f82fe8a2078c60cbfe9e8be89c055ac928
maria luiza <up202304306@edu.fe.up.pt>
2026-04-09 11:57:42 +0100

    fixed unitcubequad to have only one quad

--- tp4/MyUnitCubeQuad.js
+++ tp4/MyUnitCubeQuad.js
@@ -4,49 +4,43 @@ import { MyQuad } from "./MyQuad.js";
 export class MyUnitCubeQuad extends CGFobject {
     constructor(scene){
         super(scene);
-        // bastava um quad :)
-        this.quad1 = new MyQuad(scene);
-        this.quad2 = new MyQuad(scene);
-        this.quad3 = new MyQuad(scene);
-        this.quad4 = new MyQuad(scene);
-        this.quad5 = new MyQuad(scene);
-        this.quad6 = new MyQuad(scene);
+        this.quad = new MyQuad(scene);
     }
 
     display(){
 
         this.scene.pushMatrix();
         this.scene.translate(0,0, 0.5);
-        this.quad1.display();
+        this.quad.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
         this.scene.translate(-0.5,0,0);
         this.scene.rotate(90 * Math.PI / 180, 0, 1, 0);
-        this.quad2.display();
+        this.quad.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
         this.scene.translate(0.5,0,0);
         this.scene.rotate(-90 * Math.PI / 180, 0, 1, 0);
-        this.quad3.display();
+        this.quad.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
         this.scene.translate(0,0, -0.5);
-        this.quad4.display();
+        this.quad.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
         this.scene.translate(0,0.5,0);
         this.scene.rotate(90 * Math.PI / 180, 1, 0, 0);
-        this.quad5.display();
+        this.quad.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
         this.scene.translate(0,-0.5,0);
         this.scene.rotate(90 * Math.PI / 180, 1, 0, 0);
-        this.quad6.display();
+        this.quad.display();
         this.scene.popMatrix();
 
     }
