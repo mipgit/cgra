e9a5e923c54659c467e7cea5063d0f75afdc45c0
maria luiza <up202304306@edu.fe.up.pt>
2026-04-09 12:13:17 +0100

    added missing indices in quad to close cube

--- tp4/MyQuad.js
+++ tp4/MyQuad.js
@@ -24,7 +24,9 @@ export class MyQuad extends CGFobject {
 		//Counter-clockwise reference of vertices
 		this.indices = [
 			0, 1, 2,
-			1, 3, 2
+			1, 3, 2,
+			2, 1, 0,
+			2, 3, 1
 		];
 
 		//Facing Z positive
