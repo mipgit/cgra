16171cf5a8c90282f5b57757290b654fcce16052
Inês <up202306659@up.pt>
2026-04-15 17:23:18 +0100

    Finished grayscale task, finishing teapot exercise!

--- tp5/ShaderScene.js
+++ tp5/ShaderScene.js
@@ -89,7 +89,8 @@ export class ShaderScene extends CGFscene {
 			new CGFshader(this.gl, "shaders/texture3anim.vert", "shaders/texture3anim.frag"),
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/sepia.frag"),
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/convolution.frag"),
-			new CGFshader(this.gl, "shaders/position-color.vert", "shaders/position-color.frag")
+			new CGFshader(this.gl, "shaders/position-color.vert", "shaders/position-color.frag"),
+			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/grayscale.frag"),
 		];
 
 		// additional texture will have to be bound to texture unit 1 later, when using the shader, with "this.texture2.bind(1);"
@@ -111,7 +112,8 @@ export class ShaderScene extends CGFscene {
 			'Animation example': 6,
 			'Sepia': 7,
 			'Convolution': 8,
-			'Position-based Color': 9
+			'Position-based Color': 9,
+			'Grayscale': 10
 		};
 
 		// shader code panels references
